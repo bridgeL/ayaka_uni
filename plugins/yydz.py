1a40e8ccce04fb68282b5c3696af59d9834a5fc0
bridgeL <47290820+bridgeL@users.noreply.github.com>
2022-07-07 00:09:26 +0800

    yydz

--- plugins/yydz.py
+++ plugins/yydz.py
@@ -8,20 +8,19 @@ app.help = {
 }
 
 # 仅当idle状态时生效
+
+
 @app.command(["yydz", "一眼丁真"])
-async def handle(bot: Bot, event:GroupMessageEvent, device: AyakaDevice):
+async def handle(bot: Bot, event: GroupMessageEvent, device: AyakaDevice):
     cmd, args, arg = div_cmd_arg(["yydz", "一眼丁真"], event.message)
     if not args:
         await bot.send(event, "一眼丁真，鉴定为 啥也没输入")
     elif len(args) == 1:
         await bot.send(event, "一眼丁真，鉴定为 没得选")
     else:
-        i = randint(0,len(args)*3)
+        i = randint(0, len(args)*3)
         if i == 0:
-            ans = ["我全都要","选不选选","选，选不出来"][randint(0,2)]
+            ans = ["我全都要", "为定鉴，真丁眼一", "".join(args)][randint(0, 2)]
         else:
-            ans = args[i%len(args)]
+            ans = args[i % len(args)]
         await bot.send(event, f"一眼丁真，鉴定为 {ans}")
-
-
-
